d1617cb643e71ae27ba602af888b7d8c4f266d4c
Elias2510 <eliasmilosi2002@gmail.com>
2024-02-19 10:48:15 +0200

    .

--- obj/Debug/net7.0/test.AssemblyInfo.cs
+++ obj/Debug/net7.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44dd8510aa0db3c1315b11a5220e23e2be608629")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15ea949f8c5080df01ee1972e505ad3101cef21")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
